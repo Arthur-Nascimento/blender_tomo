e470832e4f0af381bc6994e40087a7b2dfc52163
Arthur Nascimento <art.dnascimento@gmail.com>
2024-12-12 02:09:20 -0300

    Generalizing path to save scans

--- generate_scans.py
+++ generate_scans.py
@@ -2,19 +2,24 @@ import bpy
 import os
 import numpy as np
 
+C = bpy.context
+D = bpy.data
+
 def make_scans(name='Empty', samples = 180, animation = False, slices_number = 1):
-    scans_path = './scans'
+    scans_path = os.path.join(os.getcwd(), 'scans')
     id = len(os.listdir(scans_path))
-    os.makedirs(os.path.join(scans_path, f"test{id}"), if_exists = True)
+    scans_folder = os.path.join(scans_path, f"test{id}")
+    os.makedirs(scans_folder, exist_ok = True)
+
     step = 180 / samples
     
     # Defining scanner
-    tomograph = bpy.data.objects[name]
+    tomograph = D.objects[name]
     for slice in range(slices_number):
         for i in range(samples):
             angle = i * step
             tomograph.rotation_euler.y = np.radians(angle)
-            bpy.context.scene.render.filepath = os.path.join(scans_path,f"test{id}" , f"{i}.jpg")
+            C.scene.render.filepath = os.path.join(scans_folder, f"{i}.jpg")
             bpy.ops.render.render(write_still = True)
     
     tomograph.rotation_euler.y = 0  
